57e8865bfe4b28612af6a3ef1eb45eea286def6d
Harry <harry@MacBookAir.mshome.net>
2026-01-04 02:33:09 +0100

    Added carbon intensity client

---
+++ energy_generation_mix_backend/src/test/java/energymix/ClientTest/CarbonIntensityClientTest.java
@@ -0,0 +1,32 @@
+package energymix.ClientTest;
+
+import energymix.client.CarbonIntensityClient;
+import energymix.client.dto.GenerationResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class CarbonIntensityClientTest {
+
+    @Autowired
+    CarbonIntensityClient carbonIntensityClient;
+
+    @Test
+    void checkApiData(){
+        String to = OffsetDateTime.now(ZoneOffset.UTC).withMinute(0).withSecond(0).withNano(0).toString();
+        String from = OffsetDateTime.now(ZoneOffset.UTC).withMinute(0).withSecond(0).withNano(0).minusHours(2).toString();
+
+        GenerationResponseDto responseDto = carbonIntensityClient.getGeneration(from, to);
+
+        assertNotNull(responseDto);
+        assertNotNull(responseDto.data());
+        assertFalse(responseDto.data().isEmpty());
+    }
+}
