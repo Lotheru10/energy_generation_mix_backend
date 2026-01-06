28938c57755bd3139463cef1fcb719ee46e9dca1
jprygiel <93471935+Lotheru10@users.noreply.github.com>
2026-01-06 23:54:52 +0100

    Fix syntax

--- src/main/java/energymix/controller/ChargingController.java
+++ src/main/java/energymix/controller/ChargingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "@CrossOrigin(origins = \"https://energy-mix.onrender.com\")")
+@CrossOrigin(origins = "https://energy-mix.onrender.com")
 public class ChargingController {
     private final ChargingService service;
 
